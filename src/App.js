736a80a74d941057e1431aa1adf1a62207e3c0b7
Nam-Nguyen-Dev <nguyenn.dev@gmail.com>
2023-02-10 16:43:45 -0600

    testing App component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div>
+class App extends Component {
+  constructor() {
+    super();
 
-    </div>
-  );
+    this.state = {
+      task: []
+    }
+
+    this.taskUp = this.taskUp.bind(this);
+  }
+
+  taskUp() {
+    this.setState({
+      task: this.state.task + 1
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.taskUp}>Click Me!</button>
+        <p>{this.state.task}</p>
+      </div>
+    )
+  }
 }
 
 export default App;
