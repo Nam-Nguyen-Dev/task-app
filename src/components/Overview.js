301b361d5f0d458c2c184d74b1ae677a0aa10f6c
Nam-Nguyen-Dev <nguyenn.dev@gmail.com>
2023-02-10 16:39:18 -0600

    Build Overview component

---
+++ src/components/Overview.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Overview extends Component {
+    constructor(props){
+        super(props);
+    }
+
+    render() {
+        return (
+            this.props.tasks.map((task => <h1>task</h1>))
+        )
+    }
+}
+
+export default Overview;
